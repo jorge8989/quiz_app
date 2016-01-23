e9fd26394dcc0cc9c85dc7b1ac5492e000894148
Jorge Arteaga <jorge_arteaga89@hotmail.com>
2016-01-23 05:23:19 +0000

    quiz and personalities basic validations added

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,3 +1,6 @@
 class Personality < ActiveRecord::Base
     belongs_to :quiz
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :quiz_id, presence: true
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,5 @@
 class Quiz < ActiveRecord::Base
     has_many :personalities
+    validates :name, presence: true
+    validates :description, presence: true
 end
