0c745ad02038fde4ea1e8b6e68e6f0b398c0a8aa
Jorge Arteaga <jorge_arteaga89@hotmail.com>
2016-01-23 05:18:11 +0000

    personality nested to quizzes, CRUD and forms working

--- app/controllers/personalities_controller.rb
+++ app/controllers/personalities_controller.rb
@@ -1,73 +1,54 @@
 class PersonalitiesController < ApplicationController
   before_action :set_personality, only: [:show, :edit, :update, :destroy]
 
-  # GET /personalities
-  # GET /personalities.json
-  def index
-    @personalities = Personality.all
-  end
 
-  # GET /personalities/1
-  # GET /personalities/1.json
+
   def show
   end
 
-  # GET /personalities/new
   def new
+    @quiz = Quiz.find(params[:quiz_id])
     @personality = Personality.new
   end
 
-  # GET /personalities/1/edit
+
   def edit
   end
 
-  # POST /personalities
-  # POST /personalities.json
+
   def create
-    @personality = Personality.new(personality_params)
-
-    respond_to do |format|
+    @quiz = Quiz.find(params[:quiz_id])
+    @personality = @quiz.personalities.build(personality_params)
       if @personality.save
-        format.html { redirect_to @personality, notice: 'Personality was successfully created.' }
-        format.json { render :show, status: :created, location: @personality }
+        redirect_to quiz_path(@quiz)
       else
-        format.html { render :new }
-        format.json { render json: @personality.errors, status: :unprocessable_entity }
+        render :new 
       end
-    end
   end
 
-  # PATCH/PUT /personalities/1
-  # PATCH/PUT /personalities/1.json
+
   def update
-    respond_to do |format|
       if @personality.update(personality_params)
-        format.html { redirect_to @personality, notice: 'Personality was successfully updated.' }
-        format.json { render :show, status: :ok, location: @personality }
+        redirect_to quiz_path(@personality.quiz.id), notice: 'Personality was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @personality.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
-  # DELETE /personalities/1
-  # DELETE /personalities/1.json
+
   def destroy
+    @quiz = @personality.quiz
     @personality.destroy
-    respond_to do |format|
-      format.html { redirect_to personalities_url, notice: 'Personality was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to quiz_path(@quiz.id), notice: 'Personality was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_personality
       @personality = Personality.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def personality_params
       params.require(:personality).permit(:name, :description)
     end
