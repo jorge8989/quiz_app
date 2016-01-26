a480230a0cf42acc6234bcbc576b7b30e25acab5
Jorge Arteaga <jorge_arteaga89@hotmail.com>
2016-01-26 15:08:52 +0000

    coffeescripts removed, application.js require_tree . removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require_tree .
+//= require underscore-min
+//= require quiz_processor
 
 $(function(){ $(document).foundation(); });
